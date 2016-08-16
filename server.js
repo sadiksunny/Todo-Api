e10fbd5fa25d9d817313fd3a595c72882c0644a0
Sadik Mahmud Sunny <sadik.m.sunny@gmail.com>
2016-08-16 15:20:10 -0400

    Add POST for todos'

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 var express = require ('express');
+var bodyParser = require ('body-parser');
 var app = express ();
 var PORT = process.env.PORT || 3000 ;
-var todos = [{
-	id : 1 , 
-	description : 'Meet Mom',
-	completed : false 
-}, {
-	id : 2,
-	description: 'Go to market' ,
-	completed: false 
-} , {
-	id : 3 ,
-	description: 'Washing car',
-	completed : true
-}];
+var todos = [];	
+
+
+var todoNextId = 1;
+app.use (bodyParser.json());
+
 app.get ('/' , function (req ,res){
 	res.send ('Todo send API Root');
 });
@@ -40,6 +34,18 @@ app.get ('/todos/:id' , function (req , res){
 	}
 	
 });
+
+//POST /todos/:id
+app.post ('/todos'  , function (req , res){
+	var body = req.body ;
+	body.id = todoNextId ++;
+	
+	todos.push (body);
+	
+	// console.log ('description '  + body.description);
+	res.json (body);
+});
+
 app.listen(PORT , function(){
 	console.log ("express listening on port " + PORT );
 });
