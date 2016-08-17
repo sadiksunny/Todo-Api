8881873e3c87c503d5721a371c36d085d4aaa91c
Sadik Mahmud Sunny <sadik.m.sunny@gmail.com>
2016-08-17 11:23:08 -0400

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require ('express');
 var bodyParser = require ('body-parser');
+var _ = require ('underscore');
 var app = express ();
 var PORT = process.env.PORT || 3000 ;
 var todos = [];	
@@ -20,13 +21,13 @@ app.get('/todos' , function(req , res ){
 //GET /todos/:id
 app.get ('/todos/:id' , function (req , res){
 	var todoId = parseInt(req.params.id , 10) ;
-	var matchedTodo;
-	todos.forEach (function (todo){
-		if (todoId === todo.id){
-			matchedTodo = todo;
-			return todo;
-		}
-	});
+	 var matchedTodo = _.findWhere(todos, {id: todoId});
+	// todos.forEach (function (todo){
+		// if (todoId === todo.id){
+			// matchedTodo = todo;
+			// return todo;
+		// }
+	// });
 	if (matchedTodo){
 		res.json (matchedTodo);
 	}else {
@@ -37,9 +38,17 @@ app.get ('/todos/:id' , function (req , res){
 
 //POST /todos/:id
 app.post ('/todos'  , function (req , res){
-	var body = req.body ;
-	body.id = todoNextId ++;
+	var body = _.pick( req.body  , 'description' , 'completed');
 	
+	
+	
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+	return	res.status (400).send();
+	}
+	body.description = body.description.trim();
+	//add id field
+	body.id = todoNextId ++;
+	//push todos to body
 	todos.push (body);
 	
 	// console.log ('description '  + body.description);
