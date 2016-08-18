be4b0c7c8c99f745f30c6230d4036ff07beaf8d5
Sadik Mahmud Sunny <sadik.m.sunny@gmail.com>
2016-08-18 11:33:14 -0400

    Add DELETE todos route

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var app = express ();
 var PORT = process.env.PORT || 3000 ;
 var todos = [];	
 
-
 var todoNextId = 1;
+
 app.use (bodyParser.json());
 
 app.get ('/' , function (req ,res){
@@ -53,6 +53,18 @@ app.post ('/todos'  , function (req , res){
 	
 	// console.log ('description '  + body.description);
 	res.json (body);
+});
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
 
 app.listen(PORT , function(){
